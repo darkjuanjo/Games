f6dfbe062ef6b12e2d6583006ab1b9e2e660288e
Juan Jose Paz <juanjosepazroman@hotmail.com>
2022-04-01 12:21:45 -0700

    added shop with animation frames

--- Javascript/index.js
+++ Javascript/index.js
@@ -15,6 +15,16 @@ const background = new Sprite({
     imgSrc: './img/background.png'
 })
 
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y:128
+    },
+    imgSrc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
+
 const keys = {
     a: {
         pressed: false
@@ -40,7 +50,8 @@ function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0,0, canvas.width, canvas.height);
-    background.draw();
+    background.update();
+    shop.update();
     player.update();
     enemy.update();
 
