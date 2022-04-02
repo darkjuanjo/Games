a03efbbeef1fae7c0850dc6d9bc3ec2ebeff08eb
Juan Jose Paz <juanjosepazroman@hotmail.com>
2022-04-02 00:16:10 -0700

    added jump animation

--- Javascript/index.js
+++ Javascript/index.js
@@ -39,14 +39,24 @@ function animate() {
 
     //player movement
     player.velocity.x = 0;
-    player.image = player.sprites.idle.image;
     if( keys.a.pressed && player.lastKey === 'a') 
     {
         player.velocity.x = -5;
-        player.image = player.sprites.run.image;
+        player.switchSprite('run');
     } else if (keys.d.pressed && player.lastKey === 'd') {
         player.velocity.x = 5;
-        player.image = player.sprites.run.image;
+        player.switchSprite('run');
+    }
+    else {
+        player.switchSprite('idle');
+    }
+
+    if(player.velocity.y < 0) 
+    {
+        player.switchSprite('jump');
+    } else if (player.velocity.y > 0)
+    {
+        player.switchSprite('fall');
     }
 
     //enemy movement
@@ -136,6 +146,14 @@ const player = new Fighter({
         run: {
             imgSrc: './img/samuraiMack/Run.png',
             framesMax: 8,
+        },
+        jump: {
+            imgSrc: './img/samuraiMack/Jump.png',
+            framesMax: 2,
+        },
+        fall: {
+            imgSrc: './img/samuraiMack/Fall.png',
+            framesMax: 2,
         }
     }
 });
